48bd911a5a52a9a798e3031c3b1f2be0a6ba124c
SG1515 <sunggu507@naver.com>
2022-07-18 14:14:56 +0900

    put구현 2+get 구현

--- src/main/java/com/ll/exam/HashMap.java
+++ src/main/java/com/ll/exam/HashMap.java
@@ -1,5 +1,51 @@
 package com.ll.exam;
 
-public class HashMap<K,V> { //제너릭으로 구성
+public class HashMap<K,V> {
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    HashMap() {
+        size = 0;
+        keys = new Object[100];
+        values = new Object[100];
+    }
+
+    public void put(K key, V value) {
+        int index = indexOfKey(key);
+
+        if ( index != -1) {
+            values[index] = value;
+            return;
+        }
+
+
+        keys[size] = key;
+        values[size] = value;
+
+        size++;
+
+    }
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key.equals(keys[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public V get(K key) {
+        int index = indexOfKey(key);
+
+        if (index == -1) {
+            return null; //object니까 null
+        }
+        return (V) values[index];
+    }
+
 
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,10 +2,27 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
 }
 
 class HashMapTest {
+    @Test
+    void get() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+        int age = ages.get("철수");
+
+        assertEquals(22, age);
+    }
+
+    @Test
+    void put() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+    }
+
     @Test
     void 클래스가_존재한다() {
         HashMap map = new HashMap();
